c21ecc218c0339ae60b31343b0f17da435313a45
dyavanapellisujal <dyavanapellisujal7@gmail.com>
2024-05-25 16:47:29 +0530

    csvscriptadded

---
+++ useraddcsv.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module ActiveDirectory
+
+$file = Read-Host -Prompt "Enter the Path to Csv File containing user account information"
+
+$users = Import-Csv $file
+
+ForEach ($user in $users) {
+    
+    New-ADUser `
+    -Name ($user.'FirstName ' + " " + $user.LastName )`
+    -GivenName ($user.'FirstName ')`
+    -Surname ($user.LastName)`
+    -UserPrincipalName ($user.'FirstName '+ "." + $user.LastName)`
+    -Path $user.OrganizationalUnit`
+    -Enabled ([System.Convert]::ToBoolean($user.Enabled))
+
+
+}
