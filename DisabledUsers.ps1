d8afd583489e54f2c7024032ae5b63d1f8ed6c0e
dyavanapellisujal <dyavanapellisujal7@gmail.com>
2024-05-25 17:01:35 +0530

    script for disabled users to move to disabled users OU

---
+++ DisabledUsers.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory
+
+Search-ADAccount -AccountDisabled | Select-Object Name,DistinguishedName
+
+Search-ADAccount -AccountDisabled | Move-ADObject -TargetPath "OU=Disabled user,OU=itflee,DC=itflee,DC=com"
+
+
+Get-ADuser -Filter {Enabled -eq $true} -SearchBase "OU=Disabled user,OU=itflee,DC=itflee,DC=com" | Disable-ADAccount
