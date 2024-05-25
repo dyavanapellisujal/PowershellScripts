a5652d121cfdf872922d34483ddc377b3f42f7c9
dyavanapellisujal <dyavanapellisujal7@gmail.com>
2024-05-25 16:01:16 +0530

    scripts

---
+++ Adusercreation.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module ActiveDirectory
+
+$firstname = Read-Host -Prompt "Please Enter Your Name"
+$lastname = Read-Host -Prompt "Last Name"
+$Password = Read-Host -Prompt "Password" -AsSecureString
+$Path = Read-Host -Prompt "Enter The path"
+$logonname = Read-Host -Prompt "Enter Logon name"
+
+New-ADUser `
+    -Name "$firstname $lastname" `
+    -GivenName $firstname `
+    -Surname $lastname `
+    -AccountPassword $Password `
+    -Path $Path `
+    -UserPrincipalName "$logonname" `
+    -Enabled 1 `
+    -ChangePasswordAtLogon 0
