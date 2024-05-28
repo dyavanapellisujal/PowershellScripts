37f5e5cf560571ccc20e9771b2ab99b1f8cd909a
dyavanapellisujal <dyavanapellisujal7@gmail.com>
2024-05-28 22:53:13 +0530

    eventlogfilterscript

---
+++ useradd.ps1
@@ -0,0 +1,50 @@
+﻿Import-Module ActiveDirectory
+
+$file = Read-Host -Prompt "Enter the Path to Csv File containing user account information"
+
+$users = Import-Csv $file
+
+ForEach ($user in $users) {
+    $accountnumber= verifyusername($user.'FirstName'[0] + $user.LastName)
+    $username=($user.'FirstName'[0]+$user.LastName+ $accountnumber)
+    New-ADUser `
+    -Name ($user.'FirstName ' + " " + $user.LastName )`
+    -GivenName ($user.'FirstName ')`
+    -Surname ($user.LastName)`
+    -UserPrincipalName $username `
+    -SamAccountName $username `
+    -Path $user.OrganizationalUnit`
+    -Enabled ([System.Convert]::ToBoolean($user.Enabled))
+
+
+}
+
+
+function verifyusername($username){
+    $i=1
+    if(usernametaken($username) -eq $true){
+        while (usernametaken($username + $i) -eq $true){
+            $i++
+
+        }else{
+            return ""
+        }
+        return $i
+    }
+    
+}
+
+function usernametaken($usernname){
+
+    $test1= Get-ADuser -Filter { UserPrincipalname -eq $usernname }
+    $test2= Get-Aduser -Filter { SamAccountName -eq $usernname }
+
+    if($test1 -eq $Null -and $test2 -eq $Null){
+        return false
+
+    } else {
+        return true
+     }
+
+}
+
---
+++ winlogfilter.ps1
@@ -0,0 +1,37 @@
+﻿#This script is to filter logs based on useraccount tried and failed by the account and the ID through which it failed.
+
+
+$XMLFILTER=@'
+   
+   <QueryList>
+  <Query Id="0" Path="Security">
+    <Select Path="Security">*[System[(Level=1  or Level=2 or Level=3 or Level=4 or Level=0 or Level=5) and (EventID=4625)]]</Select>
+  </Query>
+</QueryList>
+    
+'@
+
+$Events = Get-WinEvent -FilterXml $XMLFILTER
+
+$LogEntry=[System.Collections.ArrayList]@()
+
+ForEach($Event in $Events){
+    #The following for loop is for getting the index value of all the properties output by Event.properties
+    #Foreach($x in 0..$Event.Properties.Count){
+     #       Write-Host "$x - " $Event.Properties[$x].value
+    #}
+    $log = New-Object -TypeName psobject
+    $Usernametried=$Event.Properties[5].Value
+    $DomainTried=$Event.Properties[6].Value
+    $ClientNameFrom=$Event.Properties[13].Value
+    $ClientIPform=$Event.Properties[19].Value
+
+    Add-Member -InputObject $log -MemberType NoteProperty -Name "UsernameTried" -Value $Usernametried
+    Add-Member -InputObject $log -MemberType NoteProperty -Name "DomainTried" -Value $Domaintried
+    Add-Member -InputObject $log -MemberType NoteProperty -Name "ClientNameFrom" -Value $ClientNameFrom
+    Add-Member -InputObject $log -MemberType NoteProperty -Name "ClientIPFrom" -Value $ClientIPfromm
+
+    [void]$LogEntry.Add($log)
+}
+
+$log #filter the output with your requirement.
